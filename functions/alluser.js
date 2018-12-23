eff7146896e7d8e2163b658cde490a20aa3f1b01
Alex Duval <alexduval71@gmail.com>
2018-12-23 17:18:25 +0100

    clean pdf thing

--- functions/alluser.js
+++ functions/alluser.js
@@ -1,11 +1,10 @@
 exports.handler = function(event, context, callback) {
   const { name, age, nat } = JSON.parse(event.body);
-  // const body = JSON.parse(event.body);
 
   callback(null, {
     statusCode: 200,
     body: JSON.stringify({
-      message: `hello ${name}, you're ${age} from ${nat}`
+      data: `hello ${name}, you're ${age} from ${nat}`
     })
   });
 };
